b20813043701418ffdfc9a09d18cd1c6dd27f8fa
psyberd1001 <108468415+psyberd1001@users.noreply.github.com>
2024-08-10 12:30:12 +0300

    Add files via upload

---
+++ srball.py
@@ -0,0 +1,11 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+new_students = sorted(students)
+print(new_students)
+grades[0] = 4.0
+grades[1] = 2.25
+grades[2] = 4.0
+grades[3] = 3.6666666666666665
+grades[4] = 4.8
+print(grades)
+print(dict(zip(new_students, grades)))
